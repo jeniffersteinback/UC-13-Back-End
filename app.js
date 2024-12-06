ee3cafead9a819cc891c4d1a269ed52529c31962
Jeniffer Steinback <jeniffer.asnasciment@gmail.com>
2024-12-06 19:41:40 -0300

    Aula

--- app.js
+++ app.js
@@ -7,7 +7,68 @@ app.use(express.json());
 
 app.get('/', (requisicao, resposta) => {
     resposta.status(200).send(carros2024);
-
 });
 
+app.get('/:sigla', (requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtendo a Sigla
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); // Busca o carro pela sigla
+    if (!carro) {
+        // ! Vazio Not
+        // Se o carro não for enctrado, retorna erro 404
+        resposta
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!' //Mensagem de erro
+        );
+    return;
+    }
+    resposta.status(200).send(carro); // Se encontrado a sigla retorna a resposta correta
+});
+
+app.post('/', (req, res) => {
+    const novoCarro = req.body; // Obtém o corpo enviado para incluir um carro
+    carros2024.push(novoCarro); // Adiciona o novo carro à lista de carros.
+    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200 (Ok).
+});
+
+app.put('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const carroSelecionado = carros2024.find(c => c.sigla === siglaInformada);
+    if (!carroSelecionado) {
+        res
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada!'
+        );
+        return;
+    };
+    const campos = Object.keys(req.body); //Obtem o corpo da requisição
+    for (let campo of campos){
+        carroSelecionado[campo] = req.body[campo]; //Atualiza o carro com a informação
+    }
+    res.status(200).send(carroSelecionado); //Retorna a lista atualizada
+});
+
+app.delete('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const IndiceCarroSelecionado = carros2024.findIndex(
+        (c) => c.sigla === siglaInformada
+    );
+    if (IndiceCarroSelecionado === -1) {
+         res
+         .status(404)
+         .send(
+            'Não existe um carro com a sigla informada!'
+         );
+         return;
+        };
+        const carroRemovido = carros2024.splice(IndiceCarroSelecionado, 1);
+        res.status(200).send(carroRemovido);
+});
+
+// Inicia o servidor na porta 3000:
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
+
+
+// node app.js
+// localhost:3000/
